20f7d74ffce1d12aedfdc90a55fedec56bba5d55
Ulisses Ferreira <ulissesvf@gmail.com>
2019-06-19 14:58:21 +0100

    refactor: make output in the same line

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const foobar = require('./foobar')
 console.log('Hello Codacy')
 
 for (let i = 1; i < 100; i++) {
-  console.log(foobar(i) + " ");
+  process.stdout.write(foobar(i) + " ");
 }
 
-console.log(foobar(100) + "")
+process.stdout.write(foobar(100) + "\n")
